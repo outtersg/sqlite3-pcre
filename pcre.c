84713e7bb5d755382e84c0ffa7f530f13fe902bd
Guillaume Outters <guillaume-sqleur@outters.eu>
2023-01-05 06:58:26 +0100

    PCRE2
    allow libraries to link to PCRE2

--- pcre.c
+++ pcre.c
@@ -9,21 +9,48 @@
 
 #if !defined(SQLITE_CORE) || defined(SQLITE_ENABLE_PCRE)
 
+#ifndef WITH_PCRE
+#define WITH_PCRE
+#endif
+#if defined(WITH_PCRE) && !defined(WITH_PCRE2) && !defined(WITH_PCRE1)
+#define WITH_PCRE1
+#endif
+
 #include <assert.h>
 #include <stdlib.h>
 #include <string.h>
+#ifdef WITH_PCRE2
+#define PCRE2_CODE_UNIT_WIDTH 8
+#include <pcre2.h>
+#endif
+#ifdef WITH_PCRE1
 #include <pcre.h>
+#endif
 #include <sqlite3ext.h>
 SQLITE_EXTENSION_INIT1
 
 typedef struct {
     char *s;
+#ifdef WITH_PCRE2
+    pcre2_code *p;
+    pcre2_match_data *matches;
+#endif
+#ifdef WITH_PCRE1
     pcre *p;
     pcre_extra *e;
+#endif
 } cache_entry;
 
 #ifndef CACHE_SIZE
 #define CACHE_SIZE 16
+#endif
+
+#define POS_T int
+
+/* PCRE1-to-2 compatibility layer */
+#ifdef WITH_PCRE2
+#undef POS_T
+#define POS_T size_t
 #endif
 
 static
@@ -69,8 +96,15 @@ void regexp(sqlite3_context *ctx, int argc, sqlite3_value **argv)
 	else {
 	    cache_entry c;
 	    bzero(&c, sizeof(c));
+#ifdef WITH_PCRE2
+#define ERR_MAX_LENGTH 1024
+	    int errcode;
+		char err[ERR_MAX_LENGTH];
+#endif
+#ifdef WITH_PCRE1
 	    const char *err;
-	    int pos;
+#endif
+	    POS_T pos;
 		const char *re2 = re;
 		char *re2mod = NULL;
 		int options = 0;
@@ -78,7 +112,14 @@ void regexp(sqlite3_context *ctx, int argc, sqlite3_value **argv)
 		if (re[0] == '/') {
 			for (re2 = &re[strlen(re)]; --re2 > re;) {
 				switch (*re2) {
-					case 'i': options |= PCRE_CASELESS; break;
+					case 'i':
+#ifdef WITH_PCRE2
+						options |= PCRE2_CASELESS;
+#endif
+#ifdef WITH_PCRE1
+						options |= PCRE_CASELESS;
+#endif
+						break;
 					case '/':
 						pos = re2 - re;
 						if (!(re2mod = strdup(re))) {
@@ -96,30 +137,62 @@ void regexp(sqlite3_context *ctx, int argc, sqlite3_value **argv)
 			options = 0;
 		}
 breakfast:
+#ifdef WITH_PCRE2
+	    c.p = pcre2_compile((const unsigned char *)re2, strlen(re2), options, &errcode, &pos, NULL);
+#endif
+#ifdef WITH_PCRE1
 	    c.p = pcre_compile(re2, options, &err, &pos, NULL);
+#endif
 		if (re2mod) {
 			free(re2mod);
 		}
+#ifdef WITH_PCRE
 	    if (!c.p) {
+#ifdef WITH_PCRE2
+		pcre2_get_error_message(errcode, (unsigned char *)err, ERR_MAX_LENGTH);
+#endif
 		char *e2 = sqlite3_mprintf("%s: %s (offset %d)", re, err, pos);
+#endif
 		sqlite3_result_error(ctx, e2, -1);
 		sqlite3_free(e2);
 		return;
 	    }
+#ifdef WITH_PCRE2
+	    c.matches = pcre2_match_data_create_from_pattern(c.p, NULL);
+	    if (!c.matches) {
+		pcre2_code_free(c.p);
+		sqlite3_result_error(ctx, "pcre2_match_data_create_from_pattern: ENOMEM", -1);
+		return;
+	    }
+#endif
+#ifdef WITH_PCRE1
 	    c.e = pcre_study(c.p, 0, &err);
+#endif
 	    c.s = strdup(re);
 	    if (!c.s) {
 		sqlite3_result_error(ctx, "strdup: ENOMEM", -1);
+#ifdef WITH_PCRE2
+		pcre2_code_free(c.p);
+		pcre2_match_data_free(c.matches);
+#endif
+#ifdef WITH_PCRE1
 		pcre_free(c.p);
 		pcre_free(c.e);
+#endif
 		return;
 	    }
 	    i = CACHE_SIZE - 1;
 	    if (cache[i].s) {
 		free(cache[i].s);
+#ifdef WITH_PCRE2
+		pcre2_code_free(cache[i].p);
+		pcre2_match_data_free(cache[i].matches);
+#endif
+#ifdef WITH_PCRE1
 		assert(cache[i].p);
 		pcre_free(cache[i].p);
 		pcre_free(cache[i].e);
+#endif
 	    }
 	    memmove(cache + 1, cache, i * sizeof(cache_entry));
 	    cache[0] = c;
@@ -128,9 +201,16 @@ breakfast:
 
     {
 	int rc;
+#ifdef WITH_PCRE2
+	assert(cache);
+	rc = pcre2_match(cache->p, (const unsigned char *)str, strlen(str), 0, 0, cache->matches, NULL);
+	sqlite3_result_int(ctx, rc >= 0);
+#endif
+#ifdef WITH_PCRE1
 	assert(cache);
 	rc = pcre_exec(cache->p, cache->e, str, strlen(str), 0, 0, NULL, 0);
 	sqlite3_result_int(ctx, rc >= 0);
+#endif
 	return;
     }
 }
